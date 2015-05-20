5db573eb5252be8c5b447f6a20766cec051005ab
Joe <joe@ihex.me>
2015-05-20 16:29:55 +0800

    base36 encoder and decoder

--- mole/utils.py
+++ mole/utils.py
@@ -51,7 +51,29 @@ def find_modelclass(ModelClass):
     return sub_class
 
 
+def base36encode(number):
+    if not isinstance(number, int):
+        raise TypeError('number must be an integer')
+    if number < 0:
+        raise ValueError('number must be positive')
+
+    alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    base36 = ''
+    while number:
+        number, i = divmod(number, 36)
+        base36 = alphabet[i] + base36
+
+    return base36 or alphabet[0]
+
+
+def base36decode(number):
+    return int(number, 36)
+
 if __name__ == "__main__":
-    _hash = encrypt_password("c4ca4238a0b923820dcc509a6f75849b")
-    print(_hash)
-    print(verify_password("c4ca4238a0b923820dcc509a6f75849b", _hash))
+    # _hash = encrypt_password("c4ca4238a0b923820dcc509a6f75849b")
+    # print(_hash)
+    # print(verify_password("c4ca4238a0b923820dcc509a6f75849b", _hash))
+
+    print(base36encode(1412823931503067241))
+    print(base36decode('AQF8AA0006EH'))
